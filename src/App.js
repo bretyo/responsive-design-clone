26df89cf0fcbf45bbe2e93cf292616030e3023ab
Bret Fernberg <bret.fernberg@gmail.com>
2021-05-05 20:44:12 -0600

    Getting there

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import HamburgerIcon from './HamburgerIcon';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state={
+      menuBtnActive: true,
+      navShow: false
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <nav>
+          <div className={`nav-head ${this.state.navShow ? 'nav-active' : '' }`}>
+            <a><img src='https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg' alt='Start Bootsrap' /></a>
+            {this.state.menuBtnActive && <button onClick={()=>this.setState({navShow: !this.state.navShow})}>MENU {<HamburgerIcon/>}</button>}
+          </div>
+          {this.state.navShow && <ul className='nav-content'>
+            <li>SERVICES</li>
+            <li>PORTFOLIO</li>
+            <li>ABOUT</li>
+            <li>TEAM</li>
+            <li>CONTACT</li>
+          </ul>}
+        </nav>
+        <header>
+          <p>Welcome To Our Studio!</p>
+          <h1>IT'S NICE TO MEET YOU</h1>
+          <button>TELL ME MORE</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/HamburgerIcon.js
@@ -0,0 +1,10 @@
+function HamburgerIcon(){
+    return(
+        <span>
+            <div className='burger-bar'></div>
+            <div className='burger-bar'></div>
+            <div className='burger-bar'></div>
+        </span>
+    )
+}
+export default HamburgerIcon;
